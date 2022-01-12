ace34229b2099c73017fd24b870ff5e91568d267
SANDIPADHIKARI97 <adhikarisandip760@gmail.com>
2022-01-12 18:18:05 +0530

    heroku

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,13 @@
 
-    var mysong = document.getElementById("mysong");
+    var mysong = document.getElementById("mySong");
     var icon = document.getElementById("icon");
 
     icon.onclick = function () {
         if(mysong.paused){
             mysong.play();
-            icon.src = "pause.png"
+            icon.src = "img/pause.png";
         }else{
             mysong.pause();
-            icon.src = "play.png";
+            icon.src = "img/play.png";
         }
     }
